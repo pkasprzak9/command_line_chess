ffa2604a28705d7a865ae0d446f354f95a132984
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-11 22:23:27 +0200

    Implement #display_chessboard and write associated tests
    So far the method works when the chessboard is empty.

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: false
 
+require 'colorize'
+
 class Board
   attr_reader :chessboard
 
   def initialize
-    @chessboard = Array.new(8) { Array.new(8, ' ') }
+    @chessboard = Array.new(8) { Array.new(8, '') }
+  end
+
+  def display_chessboard
+    colors = %i[light_black light_white]
+    chessboard.each do |chessboard_row|
+      chessboard_row.each do |chessboard_cell|
+        if chessboard_cell == ''
+          print '   '.colorize(background: colors[0])
+          colors.rotate!
+        end
+      end
+      colors.rotate!
+      puts
+    end
   end
 end
+
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: false
 
+require 'colorize'
 require_relative '../lib/board'
 
 describe Board do
   describe '#initialize' do
     it 'creates a new chessboard' do
-      chessboard = Array.new(8) { Array.new(8, ' ') }
+      chessboard = Array.new(8) { Array.new(8, '') }
       expect(Board.new.chessboard).to eq(chessboard)
     end
+  end
+
+  describe '#display_chessboard' do
+    context 'when the chessboard is empty' do
+      subject(:board) { Board.new }
+      it 'displays the empty chessboard' do
+        lb = '   '.colorize(background: :light_black)
+        lw = '   '.colorize(background: :light_white)
+        cells = "#{lb}#{lw}#{lb}#{lw}#{lb}#{lw}#{lb}#{lw}\n#{lw}#{lb}#{lw}#{lb}#{lw}#{lb}#{lw}#{lb}\n"
+        chessboard = cells + cells + cells + cells
+        expect { board.display_chessboard }.to output(chessboard).to_stdout
+      end
+    end
+    it 'displays the chessboard with correctly allocated pieces'
   end
 end
