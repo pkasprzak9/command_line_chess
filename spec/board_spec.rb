e4bc7166a08d330e829cd0f8cffb7252f22172e8
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-13 14:29:55 +0200

    Write tests for #get_piece and #set_piece. Remove for #display_chessboard.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,38 +14,62 @@ describe Board do
     end
   end
 
-  describe '#display_chessboard' do
-    context 'when the chessboard is empty' do
-      subject(:board) { Board.new }
-      it 'displays the empty chessboard' do
-        lb = '   '.colorize(background: :light_black)
-        lw = '   '.colorize(background: :light_white)
-        cells = "#{lb}#{lw}#{lb}#{lw}#{lb}#{lw}#{lb}#{lw}\n#{lw}#{lb}#{lw}#{lb}#{lw}#{lb}#{lw}#{lb}\n"
-        chessboard = cells + cells + cells + cells
-        expect { board.display_chessboard }.to output(chessboard).to_stdout
+  describe '#set_piece' do
+    context 'when the position is valid' do
+      subject(:board) { described_class.new }
+      let(:piece) { double('piece') }
+      before do
+        allow(board).to receive(:valid_position?).and_return(true)
+      end
+      it 'sets the piece on the chessboard' do
+        position = [0, 0]
+        board.set_piece(piece, position)
+        expect(board.chessboard[0][0]).to eq(piece)
+      end
+
+      it 'returns true' do
+        position = [0, 0]
+        expect(board.set_piece(piece, position)).to be true
       end
     end
 
-    context 'when the chessboard is not empty' do
-      # rubocop:disable Style/StringConcatenation
-      def create_row(*cells)
-        cells.join + "\n"
+    context 'when the position is invalid' do
+      subject(:board) { described_class.new }
+      let(:piece) { double('piece') }
+      before do
+        allow(board).to receive(:valid_position?).and_return(false)
       end
-      # rubocop:enable Style/StringConcatenation
+      it 'does not set the piece on the chessboard' do
+        position = [0, 0]
+        board.set_piece(piece, position)
+        expect(board.chessboard[0][0]).to eq('')
+      end
+      it 'returns false' do
+        position = [0, 0]
+        expect(board.set_piece(piece, position)).to be false
+      end
+    end
+  end
 
-      subject(:board) { Board.new }
-      let(:light_black_cell) { '   '.colorize(background: :light_black) }
-      let(:light_white_cell) { '   '.colorize(background: :light_white) }
-      let(:knight_cell) { ' ♞ '.colorize(background: :light_black) }
-      it 'displays the chessboard with correctly allocated pieces' do
-        board.chessboard[0][0] = '♞'
-        row1 = create_row(knight_cell, light_white_cell, light_black_cell, light_white_cell, light_black_cell, light_white_cell, light_black_cell, light_white_cell)
-        row2 = create_row(light_white_cell, light_black_cell, light_white_cell, light_black_cell, light_white_cell, light_black_cell, light_white_cell, light_black_cell)
-        row3 = create_row(light_black_cell, light_white_cell, light_black_cell, light_white_cell, light_black_cell, light_white_cell, light_black_cell, light_white_cell)
+  describe '#get_piece' do
+    subject(:board) { board = described_class.new }
+    let(:piece) { double('piece') }
 
-        expected_output = row1 + row2 + row3 + row2 + row3 + row2 + row3 + row2
+    context 'when the position is valid' do
+      it 'returns the piece on the chessboard' do
+        allow(board).to receive(:valid_position?).and_return(true)
+        position = [0, 0]
+        board.set_piece(piece, position)
+        expect(board.get_piece(position)).to eq(piece)
+      end
+    end
 
-        expect { board.display_chessboard }.to output(expected_output).to_stdout
+    context 'when the position is invalid' do
+      it 'returns nil' do
+        allow(board).to receive(:valid_position?).and_return(false)
+        position = [0, 0]
+        board.set_piece(piece, position)
+        expect(board.get_piece(position)).to be nil
       end
     end
   end
