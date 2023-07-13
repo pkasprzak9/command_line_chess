f2596939f0bf2d0c1bffb83e477e79b35caa3eda
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-13 16:38:39 +0200

    Refactor piece.rb to be able to create colored different pieces.

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: false
 
-class Piece
-  attr_reader :color, :figure
+require 'pastel'
 
-  def initialize(color, figure = nil)
+class Piece
+  attr_reader :color, :figure, :moves
+
+  def initialize(color, figure = nil, moves = nil)
     @color = color
-    @figure = figure
+    @figure = Pastel.new.send(color.to_sym, figure)
+    @moves = moves
+  end
+
+  def self.create(color, figure)
+    klass = Object.const_get(figure.capitalize)
+    klass.new(color)
   end
 end
