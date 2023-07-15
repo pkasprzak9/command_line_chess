8c2863d02b6e486645c91a7030936aa6bb98bf80
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-15 13:48:54 +0200

    Add pawns logic that allows to move diagonally only if there is opponent there

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,9 +3,11 @@
 class Pawn < Piece
   def initialize(color)
     super(color, '♟')
-    @moves = [[1, 0], [2, 0], [1, 1], [1, -1], [-1, 0], [-2, 0], [-1, 1], [-1, -1]]
+    @moves = [[1, 0], [2, 0], [-1, 0], [-2, 0]]
+    @capturing_moves = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
   end
 
+  # rubocop:disable Metrics/AbcSize
   def possible_moves(board)
     position = board.get_position(self)
     possible_moves = []
@@ -15,6 +17,13 @@ class Pawn < Piece
       y += move[1]
       possible_moves << [x, y] if board.valid_position?(x, y)
     end
+    @capturing_moves.each do |move|
+      x, y = position
+      x += move[0]
+      y += move[1]
+      possible_moves << [x, y] if board.valid_position?(x, y) && board.occupied_by_opponent?(self, [x, y])
+    end
     possible_moves
   end
+  # rubocop:enable Metrics/AbcSize
 end
