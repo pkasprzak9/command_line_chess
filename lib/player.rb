2c678d8e64c5a426063e1eae48d06f4bf6217dd1
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-16 19:44:22 +0200

    update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: false
 
 class Player
-  attr_reader :name, :color
-  def initialize(name, color)
+  attr_reader :name, :color, :first
+  attr_accessor :points
+
+  def initialize(name, color, first: false)
     @name = name
     @color = color
     @points = 0
+    @first = first
   end
 end
