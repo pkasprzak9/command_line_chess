b308ac9026b7ed563d7fb1c694e0e1b62222d2c9
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-16 19:49:58 +0200

    write method that updates point for players

--- lib/player.rb
+++ lib/player.rb
@@ -2,12 +2,15 @@
 
 class Player
   attr_reader :name, :color, :first
-  attr_accessor :points
 
   def initialize(name, color, first: false)
     @name = name
     @color = color
     @points = 0
     @first = first
+  end
+
+  def update_points(points)
+    @points += points
   end
 end
