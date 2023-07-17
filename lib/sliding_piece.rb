be165f382488ced844b8176bce11b89f263a7067
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-17 18:44:04 +0200

    Split possible_moves into smaller method for sliding pieces

--- lib/sliding_piece.rb
+++ lib/sliding_piece.rb
@@ -5,18 +5,24 @@ class SlidingPiece < Piece
     position = board.get_position(self)
     possible_moves = []
     @moves.each do |move|
-      temp_position = position
-      multiplier = 1
-      while board.valid_position?(temp_position)
-        x = position[0] + (move[0] * multiplier)
-        y = position[1] + (move[1] * multiplier)
-        temp_position = [x, y]
-        break if board.valid_position?(temp_position) && board.occupied_by_friendly?(self, temp_position)
-
-        multiplier += 1
-        possible_moves << temp_position if board.valid_position?(temp_position)
-      end
+      possible_moves += calculate_moves_for_direction(board, position, move)
     end
     possible_moves
+  end
+
+  def calculate_moves_for_direction(board, position, move)
+    possible_moves_for_direction = []
+    temp_position = position
+    multiplier = 1
+    while board.valid_position?(temp_position)
+      x = position[0] + (move[0] * multiplier)
+      y = position[1] + (move[1] * multiplier)
+      temp_position = [x, y]
+      break if board.valid_position?(temp_position) && board.occupied_by_friendly?(self, temp_position)
+
+      multiplier += 1
+      possible_moves_for_direction << temp_position if board.valid_position?(temp_position)
+    end
+    possible_moves_for_direction
   end
 end
