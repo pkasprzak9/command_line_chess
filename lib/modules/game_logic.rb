fd9eeb02748cbaa09a18e1a3f8ffdde4d2a7f76f
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-19 17:40:24 +0200

    write method to translate chess notation to int array

--- lib/modules/game_logic.rb
+++ lib/modules/game_logic.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: false
 
 module GameLogic
+  def translate_chessnotation(position)
+    position = position.split('')
+    return unless valid_chessnotation?(position)
 
+    col = position[0]
+    row = position[1]
+    col = (col.ord - 65)
+    [row.to_i, col.to_i]
+  end
+
+  private
+
+  def valid_chessnotation?(position)
+    return true if position.length == 2 && position[0].match?(/[A-H]/) && position[1].match?(/[1-8]/)
+  end
 end
