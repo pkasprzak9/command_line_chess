56963204625f5d42ebdea9d6323efe5158a72a12
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-19 17:53:09 +0200

    Add GameLogic module to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: false
 
 require_relative 'libraries'
+require_relative 'modules/game_logic'
 
 class Game
+  include GameLogic
+
   def initialize(board = Board.new)
     @board = board
     @players = []
