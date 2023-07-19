6be35b02af23b866afd294a912f25abcc4aa2a16
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-19 14:04:35 +0200

    Add file that contains all needed libraries for game class

---
+++ lib/libraries.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This file contains the obligatory libraries for the game to run.
+
+require_relative 'board'
+require_relative 'piece'
+require_relative 'player'
+require_relative 'sliding_piece'
+require_relative 'pieces/king'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
+require_relative 'pieces/bishop'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
