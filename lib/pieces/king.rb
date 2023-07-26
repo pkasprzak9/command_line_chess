8a59721ee6e8e57bb52783232d1762534c0bf529
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-26 15:40:01 +0200

    Implement method that checks if the king is in a check

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -5,4 +5,13 @@ class King < Piece
     super(color, '♚')
     @moves = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, -1], [-1, -1], [-1, 1], [1, 1]]
   end
+
+  def checked?(board, position)
+    opponent_color = @color == 'blue' ? 'red' : 'blue'
+    opponent_pieces = board.get_pieces(opponent_color)
+    opponent_pieces.each do |piece|
+      return true if piece.possible_moves(board).include?(position)
+    end
+    false
+  end
 end
