ccb6679d834f4732565fa96ae2f27f81280f42a5
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-27 23:24:53 +0200

    update sliding_piece.rb

--- lib/sliding_piece.rb
+++ lib/sliding_piece.rb
@@ -18,6 +18,7 @@ class SlidingPiece < Piece
       x = position[0] + (move[0] * multiplier)
       y = position[1] + (move[1] * multiplier)
       temp_position = [x, y]
+      break if board.valid_position?(temp_position) && board.occupied_by_friendly?(self, temp_position)
 
       multiplier += 1
       possible_moves_for_direction << temp_position if board.valid_position?(temp_position)
