28908b007f5101ad4da44665b2f70e385c0e62e1
pkasprzak9 <pawkasprzak9@gmail.com>
2023-07-28 12:55:07 +0200

    update libraries.rb

--- lib/libraries.rb
+++ lib/libraries.rb
@@ -3,6 +3,7 @@
 # This file contains the obligatory libraries for the game to run.
 
 require_relative 'board'
+require_relative 'game'
 require_relative 'piece'
 require_relative 'player'
 require_relative 'sliding_piece'
@@ -12,3 +13,5 @@ require_relative 'pieces/rook'
 require_relative 'pieces/bishop'
 require_relative 'pieces/knight'
 require_relative 'pieces/pawn'
+require_relative 'modules/game_logic'
+require_relative 'modules/user_interface'
